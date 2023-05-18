89b420f2604b958811810dbc683afb9184358aeb
llaamaaa <lim.gabrielce@gmail.com>
2023-05-18 23:16:25 +0800

    d

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithCredential, signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import SignIn from "./SignInWithEmail";
+
+
 
 export interface ILoginPageProps {}
 
@@ -8,7 +13,7 @@ const LoginPage: React.FunctionComponent<ILoginPageProps> = (props) => {
     const auth = getAuth();
     const navigate = useNavigate();
     const [authing, setAuthing] = useState(false);
-    console.log("hi");
+    
 
     const signInWithGoogle =async () => {
         setAuthing(true);
@@ -25,6 +30,31 @@ const LoginPage: React.FunctionComponent<ILoginPageProps> = (props) => {
         
     };
 
+    const signInForm = document.getElementById('signin-form');
+
+if (signInForm) {
+  signInForm.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    const emailInput = document.getElementById('email-input') as HTMLInputElement;
+    const passwordInput = document.getElementById('password-input') as HTMLInputElement;
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    try {
+      // Sign in with email and password using Firebase
+      const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+      // Access the signed-in user data
+      const user = userCredential.user;
+      console.log('Signed in successfully:', user);
+    } catch (error) {
+      console.error('Sign-in error:', error);
+    }
+  });
+}
+
+
 
 
     return (
@@ -32,7 +62,9 @@ const LoginPage: React.FunctionComponent<ILoginPageProps> = (props) => {
             <p>Login Page</p>
             <button onClick={() => signInWithGoogle()} disabled = {authing}>
                 Sign in with Google
-
+            </button>
+            <button onClick = {() => SignIn} disabled = {authing}>
+                Sign in with email
             </button>
         </div>
     );
