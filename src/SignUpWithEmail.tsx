f89f11df1cc4942280994cf8644a780a91946710
llaamaaa <lim.gabrielce@gmail.com>
2023-05-26 04:47:21 +0800

    testing signup page

--- src/SignUpWithEmail.tsx
+++ src/SignUpWithEmail.tsx
@@ -3,10 +3,13 @@ import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from '@firebase/auth';
 
 import {auth} from './config';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-const SignIn = () => {
+const SignUpWithEmail = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const signIn = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
@@ -17,11 +20,18 @@ const SignIn = () => {
         .catch((error) => {
             console.log(error);
         });
-    }
-    return (
-        <div className='sign-in-container'>
+    };
+
+return (
+        <div>
+            <p>Login Page</p>
+            {/* <button onClick={() => signInWithGoogle()} disabled = {authing}>
+                Sign in with Google
+            </button> */}
+
+            <div className='sign-in-container'>
             <form onSubmit={signIn}>
-                <h1> Log In </h1>
+                
                 <input type="email" placeholder="Enter your email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}></input>
@@ -32,7 +42,15 @@ const SignIn = () => {
                     <button type="submit">Log In</button>
             </form>
         </div> 
-    )
+        <div>
+        <a onClick={() => navigate('/SignUpWithEmail')}>
+                Sign Up here!
+            </a>
+        </div>
+        <ToastContainer />
+        </div> 
+        
+    );
 }
 
-export default SignIn
+export default SignUpWithEmail;
