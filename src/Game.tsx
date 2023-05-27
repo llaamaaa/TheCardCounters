d20c9d8a99c65f0edb4a66fe2f9583839de2da86
llaamaaa <lim.gabrielce@gmail.com>
2023-05-27 23:37:50 +0800

    change logic of card generation

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,17 +3,36 @@ import { Card, CardSuit, CardValue, randomRange } from "./utils";
 
 const Game: React.FC = () => {
 
-    const randValue = CardValue[Object.keys(CardValue)[randomRange(12)]];
-    const randSuit = CardSuit[Object.keys(CardSuit)[randomRange(3)]];
-    const { value, suit }: Card = {value: randValue, suit: randSuit};
+    const deck: Array<Card> = [];
+    Object.values(CardValue).map(cardValue => {
+        Object.values(CardSuit).map(cardSuit => {
+            deck.push({value: cardValue as CardValue, suit: cardSuit as CardSuit});
+        })
+    })
+
+    const firstIndex = randomRange(51);
+    const firstCard = deck[firstIndex];
+    deck.splice(firstIndex, 1);
+
+    const secondIndex = randomRange(50);
+    const secondCard = deck[secondIndex];
+    deck.splice(secondIndex, 1);
+
+    const thirdIndex = randomRange(49);
+    const thirdCard = deck[thirdIndex];
+    deck.splice(thirdIndex, 1);
+
 
 
     return (
     <div>
         Game Screen
-        <a href="https://www.youtube.com/watch?v=PljDuynF-j0&pp=ygUJYmxhY2tqYWNr" target ="_blank">
-                    <img src={`src/assets/${value}_of_${suit}.png`} width={200} className="cardimg"></img>
-                </a>
+        
+            <img src={`src/assets/${firstCard.value}_of_${firstCard.suit}.png`} width={200} className="card"></img>
+            <img src={`src/assets/${secondCard.value}_of_${secondCard.suit}.png`} width={200} className="card"></img>
+            <img src={`src/assets/${thirdCard.value}_of_${thirdCard.suit}.png`} width={200} className="card"></img>
+ 
+
     </div>
     )
 
