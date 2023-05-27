f645e334a4bdb660a3cbbc68e0b19c8f3c20a852
llaamaaa <lim.gabrielce@gmail.com>
2023-05-27 21:48:34 +0800

    made game and cardcount pages

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 const PlayPage: React.FC = () => {
-    return <div>
+    const navigate = useNavigate();
+    
+    
+    return (<div>
         Play Screen
+        <div className="navButton" onClick={(() => navigate("/Game"))}>
+                    Game!
+                </div>
+         <div className="navButton" onClick={(() => navigate("/CardCount"))}>
+                    CardCount!
+                </div>
     </div>
+    
+    )
+    
 }
 
 export default PlayPage;
