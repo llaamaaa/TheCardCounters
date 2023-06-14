afb74f02b2b562305dd439448445556395e08b6d
fishshank <e0960120@u.nus.edu>
2023-06-14 22:04:01 +0800

    Message for the right move

--- src/ModalPopup.tsx
+++ src/ModalPopup.tsx
@@ -1,13 +1,42 @@
 import './ModalPopup.css'
+import { Card, CardSuit, CardValue, myRandomInts, randomRange } from "./utils";
+import data from './utils/strategy.json';
 
 interface ModalPopupProps {
     title: string;
+    firstCard: Card | undefined;
+    secondCard: Card | undefined;
+    thirdCard: Card | undefined; 
     onPressOk: () => void;
+    btnClicked: String;
 }
 
-const ModalPopup: React.FC<ModalPopupProps> = ({title, onPressOk}) => {
+interface DataProps{} {
+
+}
+
+const checkplay = (firstCard: Card | undefined,secondCard: Card|undefined,thirdCard: Card|undefined,btnClicked : String) => {
+    const x = data[firstCard["value"]]
+    /*string key = ""
+    if (x[secondCard["value"] + "_" + thirdCard["value"] in data) {
+
+    } else {
+
+    }*/
+
+    if (x[secondCard["value"] + "_" + thirdCard["value"]] == btnClicked) {
+        return "Win"
+    } else{
+return"loser hahah"
+    }
+
+}
+
+const ModalPopup: React.FC<ModalPopupProps> = ({title, onPressOk, firstCard, secondCard, thirdCard, btnClicked}) => {
+    console.log(firstCard,secondCard,thirdCard,btnClicked)
+    console.log(checkplay(firstCard,secondCard,thirdCard,btnClicked))
     return <div className='modalContainer'>
-        {title}
+        {checkplay(firstCard,secondCard,thirdCard,btnClicked)}
         <button onClick={() => onPressOk()} className='modalButton'>Ok</button>
     </div>
 }
