920b8477fcb3a0a47f96772a4e6e279f4cab8b86
llaamaaa <lim.gabrielce@gmail.com>
2023-06-17 19:23:16 +0800

    changed type of email input to text and created own email checker

--- src/Login.tsx
+++ src/Login.tsx
@@ -41,48 +41,75 @@ const LoginPage: React.FunctionComponent<ILoginPageProps> = (props) => {
     
       };
 
-    const signIn = (e: React.FormEvent) => {
+      const signIn = (e: React.FormEvent) => {
         e.preventDefault();
         setAuthing(true);
-    
-        signInWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            console.log(userCredential);
-            navigate('/');
-          })
-          .catch((error) => {
-            toast.error('💀', {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: "light",
+      
+        const isEmailInvalid = !isEmailValid(email); // Check if email is invalid
+      
+        if (!isEmailInvalid) {
+          signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              console.log(userCredential);
+              navigate('/');
+            })
+            .catch((error) => {
+              toast.error('Wrong password', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+              console.log(error);
+              setAuthing(false);
             });
-            console.log(error);
-            setAuthing(false);
+        } else {
+          toast.error('Invalid Email', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
           });
+          setAuthing(false);
+        }
       };
+      
+      const isEmailValid = (email: string) => {
+  // Perform your custom validation logic here
+  // You can use regular expressions or any other technique to validate the email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
 
 
 
     return (
-        <div>
-            
+        <div> 
             <div className='sign-in-container'>
-            <form onSubmit={signIn}>
+                <form onSubmit={signIn}>
                 <div className="inputemail">
-                    <input className="input__field" id = "email" type ='email' required
+                    <input className={`input__field ${isEmailValid(email) ? 'invalid' : ''}`} id = "email" type ='text' required
                         value={email}
-                        onChange={(e) => setEmail(e.target.value)}>
+                        onChange={(e) => setEmail(e.target.value)}
+                        onBlur={(e) => {
+                            const inputValue = e.target.value;
+                            isEmailValid(email);
+                          }}>
                     </input>
                     <label htmlFor="email" className ="input__label" >Email</label>
                     <span className="input__icon-wrapper">
                         <i className="input__icon ri eye-off-line"></i>
                     </span>
-                </div><div></div>
+                </div>
                 <div className="inputpw">
                 <input className="input__field" id = "password" type ='password' required
                     value={password}
